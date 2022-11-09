e838e0918faf41f381ae740463b0065f94d5209d
youssef labrahmi <ylabrahm@c2r9p3.1337.ma>
2022-11-09 23:10:11 +0100

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
       'nums' : ['nums'],
       'now' : ['system-ui'],
     },
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
