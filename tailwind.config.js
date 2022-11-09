4b5ea932bdf5fef145b08f932298ca220f932642
youssef labrahmi <ylabrahm@c2r9p1.1337.ma>
2022-11-09 16:45:19 +0100

    new version

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      'Stretch' : ['Stretch'],
+      'Azonix' : ['Azonix'],
+      'Barc1' : ['Barc1'],
+      'Barc2' : ['Barc2'],
+      'Qualy' : ['Qualy'],
+      'nums' : ['nums'],
+      'now' : ['system-ui'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
